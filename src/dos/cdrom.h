7dad8b9800835d58265d9c6c7d2af712a8057e29
Patryk Obara <dreamer.tan@gmail.com>
2019-09-21 17:18:09 +0200

    constexpr is not available on Steam Runtime

--- src/dos/cdrom.h
+++ src/dos/cdrom.h
@@ -56,7 +56,7 @@ typedef struct SCtrl {
 } TCtrl;
 
 inline void frames_to_msf(int frames, unsigned char *m, unsigned char *s, unsigned char *f) {
-	constexpr int cd_fps = 75;
+	const int cd_fps = 75;
 	*f = frames % cd_fps;
 	frames /= cd_fps;
 	*s = frames % 60;
@@ -65,7 +65,7 @@ inline void frames_to_msf(int frames, unsigned char *m, unsigned char *s, unsign
 }
 
 inline int msf_to_frames(unsigned char m, unsigned char s, unsigned char f) {
-	constexpr int cd_fps = 75;
+	const int cd_fps = 75;
 	return m * 60 * cd_fps + s * cd_fps + f;
 }
 
