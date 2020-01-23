1e4e7777391437d55264328d6bee17ff444e7951
Patryk Obara <dreamer.tan@gmail.com>
2020-01-23 11:54:48 +0100

    tests

---
+++ ut/another.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(another, test) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
---
+++ ut/test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+TEST(test_case_1, test_1) {
+        EXPECT_EQ(1, 1);
+        }
+
+TEST(test_case_1, test_2) {
+        EXPECT_TRUE(true);
+}
+
+TEST(test_case_1, test_3) {
+EXPECT_TRUE(true);
+}
