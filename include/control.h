05fc3547ff19ac80b52dbb6d140444ee733cc77b
Patryk Obara <dreamer.tan@gmail.com>
2020-03-17 13:41:25 +0100

    Fix effc++ warnings in control.h

--- include/control.h
+++ include/control.h
@@ -19,30 +19,15 @@
 #ifndef DOSBOX_CONTROL_H
 #define DOSBOX_CONTROL_H
 
-#ifndef DOSBOX_PROGRAMS_H
-#include "programs.h"
-#endif
-#ifndef DOSBOX_SETUP_H
-#include "setup.h"
-#endif
+#include "dosbox.h"
 
-#ifndef CH_LIST
-#define CH_LIST
+#include <cassert>
 #include <list>
-#endif
-
-#ifndef CH_VECTOR
-#define CH_VECTOR
-#include <vector>
-#endif
-
-#ifndef CH_STRING
-#define CH_STRING
 #include <string>
-#endif
+#include <vector>
 
-
-
+#include "programs.h"
+#include "setup.h"
 
 class Config{
 public:
@@ -59,11 +44,21 @@ public:
 	bool initialised;
 	std::vector<std::string> startup_params;
 	std::vector<std::string> configfiles;
-	Config(CommandLine * cmd):cmdline(cmd),secure_mode(false) {
+
+	Config(CommandLine *cmd)
+		: cmdline(cmd),
+		  sectionlist{},
+		  secure_mode(false),
+		  initialised(false)
+	{
+		assert(cmdline);
 		startup_params.push_back(cmdline->GetFileName());
 		cmdline->FillVector(startup_params);
-		initialised=false;
 	}
+
+	Config(const Config&) = delete; // prevent copying
+	Config& operator=(const Config&) = delete; // prevent assignment
+
 	~Config();
 
 	Section_line * AddSection_line(char const * const _name,void (*_initfunction)(Section*));
