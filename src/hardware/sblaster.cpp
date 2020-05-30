864ae652ab1615c56ad08edbc65c6c6026764356
kcgen <1557255+kcgen@users.noreply.github.com>
2020-05-30 13:12:28 -0700

    Rename CheckDMAEnd to represent its actual function
    CheckDMAEnd actually finishes the DMA transfer and actually doesn't
    return a boolean (or success/fail) as its name describes.
    This commit makes it fall in line with the other DMA_* functions.

--- src/hardware/sblaster.cpp
+++ src/hardware/sblaster.cpp
@@ -248,7 +248,8 @@ static const char * CardType() {
 	return types[type_id];
 }
 static void DSP_ChangeMode(DSP_MODES mode);
-static void CheckDMAEnd();
+
+static void DMA_Flush_Remaining();
 static void END_DMA_Event(Bitu);
 static void DMA_Silent_Event(Bitu val);
 static void GenerateDMASound(Bitu size);
@@ -260,7 +261,7 @@ static void DSP_SetSpeaker(bool how) {
 	sb.chan->Enable(how);
 	if (sb.speaker) {
 		PIC_RemoveEvents(DMA_Silent_Event);
-		CheckDMAEnd();
+		DMA_Flush_Remaining();
 	} else {
 		
 	}
@@ -326,7 +327,7 @@ static void DSP_DMA_CallBack(DmaChannel * chan, DMAEvent event) {
 		if (sb.mode==MODE_DMA_MASKED && sb.dma.mode!=DSP_DMA_NONE) {
 			DSP_ChangeMode(MODE_DMA);
 //			sb.mode=MODE_DMA;
-			CheckDMAEnd();
+			DMA_Flush_Remaining();
 			LOG(LOG_SB,LOG_NORMAL)("DMA unmasked,starting output, auto %d block %d",chan->autoinit,chan->basecnt);
 		}
 	}
@@ -628,7 +629,7 @@ static void END_DMA_Event(Bitu val) {
 	GenerateDMASound(val);
 }
 
-static void CheckDMAEnd(void) {
+static void DMA_Flush_Remaining() {
 	if (!sb.dma.left) return;
 	if (!sb.speaker && sb.type!=SBT_16) {
 		Bitu bigger=(sb.dma.left > sb.dma.min) ? sb.dma.min : sb.dma.left;
