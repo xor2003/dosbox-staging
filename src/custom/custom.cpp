e40a7cee63008fdb9906f9060c6c5685c4086478
xor2003 <zigor@gmx.ru>
2022-04-21 19:39:29 +0300

    latest state

--- src/custom/custom.cpp
+++ src/custom/custom.cpp
@@ -1031,17 +1031,19 @@ if (debug > 0)
         f.addcounter = m2c::counter;
         f.remcounter = 0;
         f.pointer_ = (dw *) m2c::raddr_ (ss, sp);
+     m2c::log_info("m_itiscall=%d\n",m_itiscall);
         f.itwascall = m_itiscall;
-        m_itiscall = false;
         if (m_current == m_ss.size ())
           m_ss.resize (m_current + 1);
         m_ss[m_current++] = f;
 //     m2c::log_info("ssize=%d\n",m_ss.size());
       }
+      m_itiscall = false;
   }
 
   void ShadowStack::pop (_STATE * _state)
   {
+    size_t calls=0;
     if (m2c::debug)
       {
         X86_REGREF
@@ -1057,15 +1059,33 @@ if (debug > 0)
                   log_error ("uncontrolled pop was before %x\n", tsp);
                 if (tsp <= sp)
                   m_ss[--m_current].remcounter = m2c::counter;
-                  if (m_ss[m_current].itwascall && !m_itisret)
+
+                  if (m_ss[m_current].itwascall)
                   {
-                    log_error ("~~It was call. It should be ret now but it is pop %x\n", tsp);
-                    ++m_needtoskipcall;
+                    log_error ("~~It was call %d\n",m_ss[m_current].itwascall);
                   }
+                  calls += m_ss[m_current].itwascall;
+                  log_error ("~~calls %zu\n",calls);
 
               }
 
             while (tsp < sp);
+
+                  if (m_itisret)
+                  {
+                    log_error ("~~It is ret\n");
+                    calls -= m_ss[m_current].itwascall;
+                  }
+
+            log_error ("~~Skipped calls %zu\n",calls);
+
+            if (calls)
+               {
+                    log_error ("~~It was call. It should be ret now but it is pop %x\n", tsp);
+                    m_needtoskipcall+=calls;
+                    log_error ("~~m_needtoskipcall=%d\n", m_needtoskipcall);
+               }
+
           }
       }
       m_itisret = false;
@@ -1077,13 +1097,13 @@ if (debug > 0)
       {
         X86_REGREF if (!m_ss.empty ())
           printf (" Emulated Stack dump:\n");
-        printf ("%8s %8s %4s:%4s %4s %4s %4s\n", "Alloc", "Dealloc", "cs", "ip", "sp", "Value", "Current value");
+        printf ("%4s %8s %8s %4s:%4s %4s %4s %4s\n", "Call", "Alloc", "Dealloc", "cs", "ip", "sp", "Value", "Current value");
         for (int i = m_ss.size () - 1; i >= 0; i--)
           {
             Frame f = m_ss[i];
             if (i == m_current - 1)
               printf ("  ");
-            printf ("%8x %8x %04x:%04x %4x %4x", f.addcounter, f.remcounter, f.cs, f.ip, f.sp, f.value);
+            printf ("%4d %8x %8x %04x:%04x %4x %4x", f.itwascall, f.addcounter, f.remcounter, f.cs, f.ip, f.sp, f.value);
             if (*f.pointer_ != f.value)
               printf (" %4x\n", *f.pointer_);
             else
