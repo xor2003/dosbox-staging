92f8f6ac2192d3970649c71a15f346730197a026
xor2003 <zigor@gmx.ru>
2022-04-22 18:26:09 +0300

    more

--- src/custom/src/vikings.exe_seg003.cpp
+++ src/custom/src/vikings.exe_seg003.cpp
@@ -2913,7 +2913,7 @@ cs=0xe25;eip=0x0018f4; 	T(MOV(di, *(dw*)(raddr(ds,di-0x7608))));	// 38436 mov   
 cs=0xe25;eip=0x0018f8; 	T(ADD(dx, 4));	// 38437 add     dx, 4 ;~ 0E25:18F8
 cs=0xe25;eip=0x0018fb; 	T(SHL(dx, 1));	// 38438 shl     dx, 1 ;~ 0E25:18FB
 cs=0xe25;eip=0x0018fd; 	T(ADD(di, dx));	// 38439 add     di, dx ;~ 0E25:18FD
-cs=0xe25;eip=0x0018ff; 	X(PUSH(0x0A000));	// 38440 push    0A000h ;~ 0E25:18FF
+cs=0xe25;eip=0x0018ff; 	X(PUSH((dw)0x0A000));	// 38440 push    0A000h ;~ 0E25:18FF
 cs=0xe25;eip=0x001902; 	X(POP(es));	// 38441 pop     es ;~ 0E25:1902
 loc_1e133:
 	// 6275 
